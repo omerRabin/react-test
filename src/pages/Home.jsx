78d8967abfe923054dcf849d718b4e8762c40f11
omerRabin <73130830+omerRabin@users.noreply.github.com>
2025-03-24 17:11:21 +0200

    wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,15 @@ const Home = () => {
   const data = useFetchPosts(userId);
 
   return (
-    <div style={{ direction: 'rtl', textAlign: 'right' }}>
-      <h1>דמו לבאגים בריאקט</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} placeholder='הכנס שם' />
-      <button onClick={() => setUserId(userId + 1)}>שנה משתמש</button>
-      <UserList users={data} />
-    </div>
+    <>
+      <div style={{ direction: 'rtl' }}> מזהה משתמש: {userId}</div>
+      <div style={{ direction: 'rtl', textAlign: 'right' }}>
+        <h1>דמו לבאגים בריאקט</h1>
+        <input value={name} onChange={(e) => setName(e.target.value)} placeholder='הכנס שם' />
+        <button onClick={() => setUserId(userId + 1)}>שנה משתמש</button>
+        <UserList users={data} />
+      </div>
+    </>
   );
 };
 
