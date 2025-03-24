e059d41e5732155ecd9e0fd2b05ecc2fdda0448b
omerRabin <73130830+omerRabin@users.noreply.github.com>
2025-03-24 17:38:37 +0200

    last bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,26 @@ import UserList from '../components/userList';
 const Home = () => {
   const [name, setName] = useState();
   const [userId, setUserId] = useState(1);
+  const [title, setTitle] = useState('');
   const data = useFetchPosts(userId);
 
   return (
-    <div style={{ direction: 'rtl', textAlign: 'right' }}>
-      <h1>דמו לבאגים בריאקט</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} placeholder='הכנס שם' />
-      <button onClick={() => setUserId(userId + 1)}>שנה משתמש</button>
-      <UserList users={data} />
-    </div>
+    <>
+      <div style={{ direction: 'rtl', textAlign: 'right' }}>{title}</div>
+      <div style={{ direction: 'rtl', textAlign: 'right' }}>
+        <h1>דמו לבאגים בריאקט</h1>
+        <input value={name} onChange={(e) => setName(e.target.value)} placeholder='הכנס שם' />
+        <button
+          onClick={() => {
+            setUserId(userId + 1);
+            setTitle(`המשתמש הבא עם מזהה ${userId}`);
+          }}
+        >
+          שנה משתמש
+        </button>
+        <UserList users={data} />
+      </div>
+    </>
   );
 };
 
